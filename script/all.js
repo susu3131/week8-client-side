5790ee9844b27949eb8e64a3363e9f7e4f6c9c93
susu3131 <anny81327@gmail.com>
2022-12-09 22:53:28 +0800

    購物車js

--- script/all.js
+++ script/all.js
@@ -1,6 +1,11 @@
 const productList = document.querySelector(".productWrap") //選取產品列表
-let productListData = []; //產品資料
+const productSelect = document.querySelector(".productSelect") //選取產品列表
+const shoppingCart = document.querySelector(".shoppingCart-tablebody") //選取購物車
+const shoppingCartList = document.querySelector(".shoppingCart")
 
+
+let cartData = []; //購物車資料
+let productListData = []; //產品資料
 
 
 // 取得產品列表
@@ -8,53 +13,164 @@ function getProductList() {
   axios.get(getProductsUrl)
     .then(function (response) {
       productListData = response.data.products;
-      console.log(productListData)
-      //1. 新增字串
-      let str = ""
-      //2. foreach處理資料 放新增的資料
-      productListData.forEach(function (item) {
-        str += `<li class="productCard">
-          <h4 class="productType">新品</h4>
-          <img src="${item.images}"  alt="">
-          <a href="#" class="addCardBtn">加入購物車</a>
-          <h3>${item.title}</h3>
-          <del class="originPrice">NT$${item.origin_price}</del>
-          <p class="nowPrice">NT$${item.price}</p>
-        </li>`
-      })
-      //3.渲染畫面
-      productList.innerHTML =str;
+      //顯示資料 渲染畫面
+      renderProductList()
+
     })
 }
 
-//2. 初始化
+//初始化
 function init() {
   getProductList();
-
+  getCartList()
 }
 init();
 
+//顯示全部資料
+function renderProductList() {
+  //1. 新增字串
+  let str = ""
+  //2. foreach處理資料 放新增的資料
+  productListData.forEach(function (item) {
+    str += strRefactor(item);
+  })
+  productList.innerHTML = str;
+}
 
+//字串組合- 產品列表(優化程式碼)
+function strRefactor(item) {
+  return `<li class="productCard">
+        <h4 class="productType">新品</h4>
+        <img src="${item.images}"  alt="">
+        <a href="#" class="addCardBtn" data-id="${item.id}">加入購物車</a>
+        <h3>${item.title}</h3>
+        <del class="originPrice">NT$${item.origin_price}</del>
+        <p class="nowPrice">NT$${item.price}</p>
+      </li>`
 
+}
 
-
-
-// C3.js
-let chart = c3.generate({
-  bindto: '#chart', // HTML 元素綁定
-  data: {
-    type: "pie",
-    columns: [
-      ['Louvre 雙人床架', 1],
-      ['Antony 雙人床架', 2],
-      ['Anty 雙人床架', 3],
-      ['其他', 4],
-    ],
-    colors: {
-      "Louvre 雙人床架": "#DACBFF",
-      "Antony 雙人床架": "#9D7FEA",
-      "Anty 雙人床架": "#5434A7",
-      "其他": "#301E5F",
+//篩選產品
+productSelect.addEventListener('change', function (e) {
+  const category = e.target.value;
+  if (category == "全部") {
+    renderProductList();
+    return; //中止程式
+  }
+  let str = "";
+  productListData.forEach(function (item) {
+    if (item.category == category) {
+      str += strRefactor(item);
     }
-  },
-});
+  })
+  productList.innerHTML = str;
+
+})
+
+
+//加入購物車
+productList.addEventListener("click", function (e) {
+  e.preventDefault(); //取消網站預設事件
+
+
+  let addBtn = e.target.getAttribute("class")
+  if (addBtn !== "addCardBtn") {
+    return
+  }
+  let productId = e.target.getAttribute("data-id") //當點擊加入購物車，取得訂單id
+  console.log(productId)
+  let num = 1;
+  //假如取得的id 跟購物車的id相同，則表示已有相同存在購物車，則預設+1
+  cartData.forEach(function (item) {
+    if (item.product.id === productId) {
+      num = item.quantity++;
+    }
+  })
+  //post購物車資料
+  axios.post(getCartsUrl, {
+    "data": {
+      "productId": productId,
+      "quantity": num
+    }
+  }).then(function (res) {
+    console.log(res);
+    rendershoppingCart();
+    init();
+    alert("加入購物車")
+  })
+
+
+})
+
+//取得購物車資料
+function getCartList() {
+  axios.get(getCartsUrl)
+    .then(function (res) {
+      cartData = res.data.carts;
+      rendershoppingCart();
+    })
+
+
+}
+
+//購物車資料
+function rendershoppingCart() {
+  //1. 新增字串
+  let str = ""
+  //2. foreach處理資料 放新增的資料
+  cartData.forEach(function (item) {
+    str += strCarts(item);
+  })
+  shoppingCart.innerHTML = str;
+}
+
+//字串組合- 購物車列表(優化程式碼)
+function strCarts(item) {
+  return `               
+<tr>
+  <td>
+      <div class="cardItem-title">
+          <img src="./photo/HvT3zlU.png" alt="">
+          <p>${item.product.title}</p>
+      </div>
+  </td>
+  <td>${item.product.price}</td>
+  <td>${item.quantity}</td>
+  <td>${item.product.price * item.quantity}</td>
+  <td class="discardBtn">
+      <a href="#" class="material-icons" data-id="${item.id}">
+          clear
+      </a>
+  </td>
+</tr>
+
+`
+
+}
+
+
+//監聽、刪除購物車資料
+shoppingCartList.addEventListener("click", function (e) {
+  e.preventDefault();
+  const card_id = e.target.getAttribute("data-id")
+  const cartDeleteBtn = e.target.getAttribute("class")
+  console.log(card_id);
+
+  if (cartDeleteBtn == "discardAllBtn") {
+    axios.delete(getCartsUrl)
+      .then(function (res) {
+        alert('刪除所有購物車資料')
+        rendershoppingCart();
+        init();
+      })
+  } 
+  else if (card_id !== null) {
+    axios.delete(`${getCartsUrl}/${card_id}`)
+      .then(function (res) {
+        alert('刪除單筆購物車資料');
+        rendershoppingCart();
+        init();
+      })
+  }
+
+})
--- script/config.js
+++ script/config.js
@@ -2,6 +2,9 @@
 const api_path = "susu3131"
 const token = "woxT6CYejdU9GWsE7DOgh6Uq3S62"
 
-
+//產品api
 const getProductsUrl = `https://livejs-api.hexschool.io/api/livejs/v1/customer/${api_path}/products`; //取得產品資料
+
+//購物車api
 const getCartsUrl = `https://livejs-api.hexschool.io/api/livejs/v1/customer/${api_path}/carts`; //取得購物車資料
+
